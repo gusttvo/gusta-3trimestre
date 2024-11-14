8bda9788e95e9dc56adcb1568b9888f2c191887b
gusttvo <141937276+gusttvo@users.noreply.github.com>
2024-11-14 16:24:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Racionais MCs',
+    ' Quem são os integrantes dos Racionais MCs?',
+    'Mano Brown, Edi Rock, Ice Blue e KL Jay.'
+)
+
+criaCartao(
+    'Hungria HipHop',
+    'Qual é o nome verdadeiro de Hungria Hip Hop?',
+    'Seu nome verdadeiro é Gabriel Novaes.'
+)
+
+criaCartao(
+    'Sabotage',
+    'Onde Sabotage nasceu?',
+    'Ele nasceu em São Paulo, no bairro de São Miguel Paulista.'
+)
+
+criaCartao(
+    'Emicida',
+    'Quando Emicida começou sua carreira?',
+    'Emicida começou sua carreira no rap em 2006'
+)
